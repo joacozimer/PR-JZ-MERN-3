a602e2e14563b433ae4483d21bc2881169777d7d
joacozimer <joacozimerman@gmail.com>
2021-10-06 17:45:39 -0300

    primer commit

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/users/singin', (req, res) => {
-    res.render('/users/singin');
+    res.render('users/singin');
 });
 
-router.get('users/singup', (req, res) =>{
+router.get('/users/singup', (req, res) =>{
     res.render('users/singup');
 });
 
